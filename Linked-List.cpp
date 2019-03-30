723a4235e89de813aef8bfc630270535ba4bfb4e
maulanataufiq30 <48612593+maulanataufiq30@users.noreply.github.com>
2019-03-30 23:24:38 +0700

    awal new

---
+++ Linked-List.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+struct node
+{
+	int data;
+	node *next;	
+};
+class list
+{
+		private:
+		node *head, *tail;
+		public:
+		list()
+		{
+			head=NULL;
+			tail=NULL;
+		}
+		void createnode(int value)
+		{
+			node *temp=new node;
+			temp->data=value;
+			temp->next=NULL;
+			if(head==NULL)
+			{
+				head=temp;
+				tail=temp;
+				temp=NULL;
+			}
+			else
+			{	
+				tail->next=temp;
+				tail=temp;
+			}
+		}
+		void display()
+		{
+			node *temp=new node;
+			temp=head;
+			while(temp!=NULL)
+			{
+				cout<<temp->data<<"\t";
+				temp=temp->next;
+			}
+		}

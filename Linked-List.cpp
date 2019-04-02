f39017eac4d1d5911755dce4772ab601343c972a
azizalhaddad <48709776+azizalhaddad@users.noreply.github.com>
2019-04-02 18:47:32 +0700

    Update Linked-List.cpp

--- Linked-List.cpp
+++ Linked-List.cpp
@@ -41,4 +41,46 @@ class list
 				cout<<temp->data<<"\t";
 				temp=temp->next;
 			}
+		}void insert_start(int value)
+		{
+			node *temp=new node;
+			temp->data=value;
+			temp->next=head;
+			head=temp;
+		}
+		void insert_position(int pos, int value)
+		{
+			node *pre=new node;
+			node *cur=new node;
+			node *temp=new node;
+			cur=head;
+			for(int i=1;i<pos;i++)
+			{
+				pre=cur;
+				cur=cur->next;
+			}
+			temp->data=value;
+			pre->next=temp;	
+			temp->next=cur;
+		}
+		void delete_first()
+		{
+			node *temp=new node;
+			temp=head;
+			head=head->next;
+			delete temp;
+		}
+		void delete_last()
+		{
+			node *current=new node;
+			node *previous=new node;
+			current=head;
+			while(current->next!=NULL)
+			{
+				previous=current;
+				current=current->next;	
+			}
+			tail=previous;
+			previous->next=NULL;
+			delete current;
 		}
